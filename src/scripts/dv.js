c166ba01731ae5bd938d831a773dc3e1f45b6d99
Olufemi Adeyemi <olufemi.segun.adeyemi@desy.de>
2017-05-29 23:05:23 +0200

    change of qos. Adjusted to reflect real index number for the context menu. Also, changed app.$.homeDir to app.$.homedir so that the indext number can be propagated appropraitely. THIS IS A MAJOR BUG, PLEASE FIX!

--- src/scripts/dv.js
+++ src/scripts/dv.js
@@ -260,9 +260,9 @@
         if (app.$.metadata.selected == 'drawer'){
             app.$.metadata.querySelector('file-metadata').currentQos = status;
         }
-        app.$.homeDir.querySelector('#feList')
-            .set('items.'+itemIndex+'.currentQos', status);
-        app.$.homeDir.querySelector('#feList').notifyPath('items.'+itemIndex+'.currentQos');
+        app.$.homedir.querySelector('#feList')
+            .set('items.'+ itemIndex + '.currentQos', status);
+        app.$.homedir.querySelector('#feList').notifyPath('items.' + itemIndex + '.currentQos');
     }
 
     function periodicalCurrentQosRequest(options)
@@ -275,15 +275,15 @@
 
                 //ask every two seconds
                 setTimeout(periodicalCurrentQosRequest(options), 2000);
-            } else if (req.response.currentQos == options.targetQos) {
+            } else if (req.response.currentQos === options.targetQos) {
                 updateFeListAndMetaDataDrawer(req.response.currentQos, options.itemIndex);
 
                 app.$.toast.text = "Transition complete! ";
                 app.$.toast.show();
             } else {
-                updateFeListAndMetaDataDrawer(options.currentQos, options.itemIndex);
+                updateFeListAndMetaDataDrawer(req.response.currentQos, options.itemIndex);
 
-                app.$.toast.text = "Transition terminated. ";
+                app.$.toast.text = "QOS changed to " + req.response.currentQos +'. ';
                 app.$.toast.show();
             }
         }).catch(
