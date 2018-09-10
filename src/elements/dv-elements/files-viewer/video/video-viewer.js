f57537d11140e10b17943383cba676033952013c
Olufemi Adeyemi <olufemi.segun.adeyemi@desy.de>
2018-09-10 22:59:22 +0200

    add with and height to the video tag

--- src/elements/dv-elements/files-viewer/video/video-viewer.js
+++ src/elements/dv-elements/files-viewer/video/video-viewer.js
@@ -3,9 +3,19 @@ class VideoViewer extends Polymer.Element
     constructor(src)
     {
         super();
+
         if (src) this.src = src;
         this._stopListener = this._stop.bind(this);
     }
+    /*ready()
+    {
+        Polymer.RenderStatus.afterNextRender(this, () => {
+            const parentWidth = this.parentNode.offsetWidth;
+            const parentHeight = this.parentNode.offsetHeight;
+
+            //this._width =;
+        })
+    }*/
     static get is()
     {
         return 'video-viewer';
@@ -16,12 +26,21 @@ class VideoViewer extends Polymer.Element
             src: {
                 type: String,
                 notify: true
+            },
+            _width: {
+                type: Number,
+                notify: true
+            },
+            _height: {
+                type: Number,
+                notify: true
             }
         }
     }
     connectedCallback()
     {
         super.connectedCallback();
+        this.$.video.addEventListener('loadeddata', this._setDimension.bind(this));
         window.addEventListener('dv-namespace-close-files-viewer-appliance', this._stopListener);
     }
     disconnectedCallback()
@@ -36,8 +55,29 @@ class VideoViewer extends Polymer.Element
     _load(src)
     {
         this.$.video.src = src;
+    }
+    _setDimension()
+    {
+        const parentWidth = this.parentNode.offsetWidth;
+        const parentHeight = this.parentNode.offsetHeight;
+        const videoWidth = this.$.video.videoWidth;
+        const videoHeight = this.$.video.videoHeight;
+
+        if (parentWidth < videoWidth && parentHeight > videoHeight) {
+            this._width = 0.8 * parentWidth;
+            this._height = (this._width * videoHeight)/videoWidth;
+        } else if (parentHeight < videoHeight &&
+            (parentWidth > videoWidth || parentWidth < videoWidth)) {
+            this._height = 0.8 * parentHeight;
+            this._width = (this._height * videoWidth)/videoHeight;
+        } else {
+            this._width = videoWidth;
+            this._height = videoHeight;
+        }
+        this.$.video.classList.remove('none');
         this.dispatchEvent(
-            new CustomEvent('dv-namespace-files-viewer-finished-loading', {bubbles:true, composed:true}));
+            new CustomEvent('dv-namespace-files-viewer-finished-loading', {bubbles:true, composed:true})
+        );
         this.$.video.play();
     }
     _stop()
